14c8a3ff71cbb0b984bf396400e79fa9ad9638af
soumyajitghosh22 <soumyaghosh257@gmail.com>
2026-02-03 14:16:11 +0530

    Update emailscript.js

--- public/js/emailscript.js
+++ public/js/emailscript.js
@@ -1,1 +1,11 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+    }
+
+    
+    emailjs.sendMail("service_m2bhx0d","template_0v9l8jw",parms)
+
+}
+
 
