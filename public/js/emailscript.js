23409f8fc2c754562a9a7b55173b1e4c0b554964
soumyajitghosh22 <soumyaghosh257@gmail.com>
2026-02-03 16:15:34 +0530

    Update emailscript.js

--- public/js/emailscript.js
+++ public/js/emailscript.js
@@ -1,11 +1,19 @@
-function sendMail(){
-    let parms = {
-        name : document.getElementById("name").value,
-    }
+const btn = document.getElementById('yes-button');
 
-    
-    emailjs.sendMail("service_m2bhx0d","template_0v9l8jw",parms)
+document.getElementById('form')
+ .addEventListener('click', function(event) {
+   event.preventDefault();
 
-}
+   btn.value = 'Sending...';
 
+   const serviceID = 'default_service';
+   const templateID = 'template_0v9l8jw';
 
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.id = 'yes-button';
+    }, (err) => {
+      btn.id = 'yes-button';
+      alert(JSON.stringify(err));
+    });
+});
